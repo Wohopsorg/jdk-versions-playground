1fff66e384e32f41ab862e993b0abd560b8fdd30
Michael Gumowski <michael.gumowski@sonarsource.com>
2023-04-14 09:10:08 +0200

    Migrate JDK18 module to JDK19

--- playground-jdk19/src/main/java/org/sonarsource/java/PatternMatchingForSwitch.java
+++ playground-jdk19/src/main/java/org/sonarsource/java/PatternMatchingForSwitch.java
@@ -43,7 +43,7 @@ public interface PatternMatchingForSwitch {
     return switch (shape) {
       case null -> "null case";
       case Triangle t -> String.format("Triangle (%d,%d,%d)", t.a(), t.b(), t.c());
-      case Rectangle r && r.volume() > 42 -> String.format("Big rectangle of volume %d!", r.volume());
+      case Rectangle r when r.volume() > 42 -> String.format("Big rectangle of volume %d!", r.volume());
       case Square s -> String.format("Square %s!", s);
       case Rectangle r -> String.format("Rectangle (%d,%d)", r.base, r.height);
       default -> "unreachable default case...";
